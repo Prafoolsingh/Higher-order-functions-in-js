5bb3f2b9d8706cea42fa12926090834780b20ff1
prafool singh <singhpraful61@gmail.com>
2024-04-16 13:02:06 +0530

    some() in js

---
+++ 5_some.js
@@ -0,0 +1,6 @@
+// some() method :Checks if at least one item in the array passes the test implemented by the provided function
+
+let arr=[1,2,3,5,7]
+
+    const hasEven = arr.some(num => num % 2 === 0);
+    console.log(hasEven)
