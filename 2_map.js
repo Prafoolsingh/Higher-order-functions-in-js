72482853999be08b6f12af6ef30f415707e6aad4
prafool singh <singhpraful61@gmail.com>
2024-04-16 12:54:49 +0530

    map() in js

---
+++ 2_map.js
@@ -0,0 +1,9 @@
+// map(): returns a new array by applying a function to each item in the original array.
+
+let arr1=[1,2,3,4,5]
+
+const arr2=arr1.map((val)=>{
+ return val*val
+})
+
+console.log(arr2);
