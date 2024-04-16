cdd854d48c2f963aacaf9a346ca7f8164f24d0c5
prafool singh <singhpraful61@gmail.com>
2024-04-16 13:19:13 +0530

    every() in js

---
+++ 6_every.js
@@ -0,0 +1,7 @@
+// every () method : Checks if all items in the array pass the test implemented by the provided function. 
+
+let arr=[2,4,5,6,7]
+
+    const everyEven = arr.every(val => val % 2 === 0);
+
+    console.log(everyEven)
